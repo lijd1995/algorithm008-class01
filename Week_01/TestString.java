0b6379a5b5d30b5551c7c91e6e0ce6cd95e4ffa6
05584 <lijunda1@msok.com>
2020-04-20 17:51:46 +0800

    111

---
+++ Week_01/TestString.java
@@ -0,0 +1,12 @@
+/**
+ * @program: Week_01
+ * @description:
+ * @author: ljd
+ * @create: 2020-04-20 12:40
+ */
+public class TestString {
+    public static void main(String[] args) {
+        String s = "a" + "b";
+        s = s + "c";
+    }
+}
